cb8830af2d93f2920cf9e9294454cb451312c56c
Danish Nisar <danishnisar.aptechnn@gmail.com>
2018-09-17 16:28:15 +0500

    Initial Commit

---
+++ MVVM Part 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVM Part 1
+//
+//  Created by MacBook Prp on 17/09/2018.
+//  Copyright © 2018 Native Brains. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
